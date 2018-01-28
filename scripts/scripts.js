478538391a7e27205a6b70425b79d17e45df5413
Kris <kristapsrss@gmail.com>
2018-01-28 21:35:18 +0200

    Add flip animation to each card

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,10 @@
-let maxCards = 16;
+let maxCards = 8;
+
+const faceImages = ["images/1.png","images/2.png","images/3.png","images/4.png",
+					"images/1.png","images/2.png","images/3.png","images/4.png"];
+
+
+
 
 const cardContainer = $("<div/>", {
 	"class" : "card-container"
@@ -16,3 +22,33 @@ for(let i = 0; i < maxCards; i++){
 	let cardWithContainer = cardContainer.clone().append(card);
 	$("#card-list").append(cardWithContainer);
 };
+
+
+
+function cardFlip(card,frontImg){
+    const cardSize = 120;
+
+    $(card).animate({
+        width : 0,
+        marginLeft : cardSize / 2,
+        marginRight : cardSize / 2
+    },{
+        complete : function(){
+            $(card).attr("src", frontImg);
+            $(card).animate({
+                width : cardSize,
+                marginLeft : 0,
+                marginRight : 0
+            })
+        }
+    })
+};
+
+
+
+
+
+$(".game-card").on("click",function(){
+	let faceImage = faceImages[$('.game-card').index(this)];
+	cardFlip(this,faceImage);
+}); 
